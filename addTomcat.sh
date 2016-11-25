615a3ac1bf8caa2b0917f5ebc05949ee7cad2cff
wangxiHome <wangxi950709@gmail.com>
2016-11-25 17:51:26 +0800

    delete redundance data

--- addTomcat.sh
+++ addTomcat.sh
@@ -1,3 +1,2 @@
-#!/bash/bin
-cp ~/.m2/repository/com/bigchat/www/bigchat_webservice/1.0-SNAPSHOT/bigchat_webservice-1.0-SNAPSHOT.war ~/soft/apache-tomcat-8.0.28/webapps/
+cp /home/wang/.m2/repository/com/bigchat/www/bigchat_webservice/1.0-SNAPSHOT/bigchat_webservice-1.0-SNAPSHOT.war /home/wang/Soft/apache-tomcat-8.0.37/webapps
 
